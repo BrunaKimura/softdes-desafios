d5b7d4c167af7a760e732d469bbd1923ac07dab4
Martim <martimfj@al.insper.edu.br>
2020-03-30 22:06:41 -0300

    Testes e Documentação

--- tests/interface.py
+++ tests/interface.py
@@ -1,12 +1,47 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_success_login(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/')
+        assert driver.title == "SoftDes 2018.2"
+
+    def test_fail_login(self):
+        driver = self.driver
+        driver.get('http://admin:batata@localhost:8080/')
+        driver.switch_to.alert.dismiss()
+        assert driver.title != "SoftDes 2018.2"
+
+    def test_success_pw_change(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/pass')
+        old_pw_input = driver.find_element_by_id('velha').send_keys("admin")
+        new_pw_input = driver.find_element_by_id('nova').send_keys("admin")
+        confirm_pw_input = driver.find_element_by_id('repeteco').send_keys("admin")
+        driver.find_element_by_id("change").click()
+        mensagem = driver.find_element_by_id("aviso-senha").text
+        assert mensagem == "Senha alterada com sucesso"
+
+    def test_fail_pw_change(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/pass')
+        old_pw_input = driver.find_element_by_id('velha').send_keys("admin")
+        new_pw_input = driver.find_element_by_id('nova').send_keys("batata")
+        confirm_pw_input = driver.find_element_by_id('repeteco').send_keys("linguiça")
+        driver.find_element_by_id("change").click()
+        mensagem = driver.find_element_by_id("aviso-senha").text
+        assert mensagem == "As novas senhas nao batem"
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
