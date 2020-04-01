35bd9ccd140d04498064dbec824cd6dadc6f013a
Martim <martimfj@al.insper.edu.br>
2020-04-01 20:35:49 -0300

    Selenium tests done

--- src/add_quiz.py
+++ src/add_quiz.py
@@ -1,17 +1,23 @@
 import sqlite3
 
 
-def add_quiz(user, pwd, type):
+def add_quiz(numb, release, expire, problem, tests, results, diagnosis):
     conn = sqlite3.connect('quiz.db')
     cursor = conn.cursor()
-    cursor.execute('Insert into QUIZ(numb, release, expire, problem, tests, results, diagnosis) values ("{1}","{2}","{3}","{4}","{5}","{6}","{7}");'.format(numb, release, expire, problem, tests, results, diagnosis))
+    cursor.execute(
+        "Insert into QUIZ(numb, release, expire, problem, tests, results, diagnosis) values ({0}, '{1}','{2}','{3}','{4}','{5}','{6}')".format(numb, release, expire, problem, tests, results, diagnosis))
     conn.commit()
     conn.close()
 
+# quizes = pd.read_csv("quiz.csv")
+# print(quizes)
+# #
+# # with open('quiz.csv', 'r') as file:
+# #     lines = file.read().splitlines()
+# #
+# # for quiz in lines:
+# #     print(eval(quiz))
+# #     numb, release, expire, problem, tests, results, diagnosis = quiz
+# #     add_quiz(numb, release, expire, problem, tests, results, diagnosis)
 
-with open('quiz.csv', 'r') as file:
-    lines = file.read().splitlines()
-
-for quiz in lines:
-    (numb, release, expire, problem, tests, results, diagnosis) = quiz.split(',')
-    add_quiz(numb, release, expire, problem, tests, results, diagnosis)
+add_quiz(2, '2020-05-04', '2020-12-31 23:59:59', 'Exemplo de problema da bruna hehe', '[[1],[2],[3]]', '[1, 2, 3]', '["nao deu 1","nao deu 2","nao deu 3"]')
--- tests/interface.py
+++ tests/interface.py
@@ -1,7 +1,11 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import os
+abs_path = os.path.abspath(".")
 
 class PythonOrgSearch(unittest.TestCase):
 
@@ -39,6 +43,44 @@ class PythonOrgSearch(unittest.TestCase):
         mensagem = driver.find_element_by_id("aviso-senha").text
         assert mensagem == "As novas senhas nao batem"
 
+    def test_submit_success_test(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/?ID=1')
+        file_path = f"{abs_path}/desafio_test1.py"
+        driver.find_element_by_id('resposta').send_keys(file_path)
+        driver.find_element_by_id('enviar-resposta').click()
+        resultado = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[3]')))
+        assert resultado.text == "OK!"
+
+    def test_submit_failed_test(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/?ID=1')
+        file_path = f"{abs_path}/desafio_test2.py"
+        driver.find_element_by_id('resposta').send_keys(file_path)
+        driver.find_element_by_id('enviar-resposta').click()
+        resultado = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[3]')))
+        assert resultado.text == "Erro"
+
+    def test_submit_failed_test_with_feedback(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/?ID=1')
+        file_path = f"{abs_path}/desafio_test2.py"
+        driver.find_element_by_id('resposta').send_keys(file_path)
+        driver.find_element_by_id('enviar-resposta').click()
+        resultado = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[3]')))
+        feedback = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[2]')))
+        assert resultado.text == "Erro" and feedback.text == "a b c"
+
+
+    def test_submit_failed_function_with_feedback(self):
+        driver = self.driver
+        driver.get('http://admin:admin@localhost:8080/?ID=1')
+        file_path = f"{abs_path}/desafio_test3.py"
+        driver.find_element_by_id('resposta').send_keys(file_path)
+        driver.find_element_by_id('enviar-resposta').click()
+        resultado = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[3]')))
+        feedback = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//table/tbody/tr[1]/td[2]')))
+        assert resultado.text == "Erro" and feedback.text == "Nome da função inválido. Usar 'def desafio1(...)'"
 
     def tearDown(self):
         self.driver.close()
