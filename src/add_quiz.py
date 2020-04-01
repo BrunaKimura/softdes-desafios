a0a4ac7b1f0079d326cbd51eb18d788cce0b7712
Martim <martimfj@al.insper.edu.br>
2020-04-01 20:36:53 -0300

    Selenium tests done

--- src/add_quiz.py
+++ src/add_quiz.py
@@ -9,15 +9,4 @@ def add_quiz(numb, release, expire, problem, tests, results, diagnosis):
     conn.commit()
     conn.close()
 
-# quizes = pd.read_csv("quiz.csv")
-# print(quizes)
-# #
-# # with open('quiz.csv', 'r') as file:
-# #     lines = file.read().splitlines()
-# #
-# # for quiz in lines:
-# #     print(eval(quiz))
-# #     numb, release, expire, problem, tests, results, diagnosis = quiz
-# #     add_quiz(numb, release, expire, problem, tests, results, diagnosis)
-
 add_quiz(2, '2020-05-04', '2020-12-31 23:59:59', 'Exemplo de problema da bruna hehe', '[[1],[2],[3]]', '[1, 2, 3]', '["nao deu 1","nao deu 2","nao deu 3"]')
